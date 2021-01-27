029f97bbce6cac9885df139e68e9911e11659ea3
jinook929 <jinook929@gmail.com>
2021-01-27 15:52:17 -0500

    commit tree da841f35e964df5e7e691d5dfe4a08e64c2dfd8a

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,17 @@
 
 
+class Course
+    @@all = []
+    def self.all
+        @@all
+    end
+
+    attr_accessor :title, :schedule, :description
+    def initialize
+        self.class.all.push(self)
+    end
+
+    def self.reset_all
+        self.all.clear
+    end
+end
